f8dd5f696b7d98545013441baddafdc76de84c4a
Chris Laprun <metacosm@gmail.com>
2018-04-19 12:18:33 +0200

    Colorize messages based on 'from' field to better visualize things.

--- greeting-service/src/main/resources/static/worker.js
+++ greeting-service/src/main/resources/static/worker.js
@@ -2,7 +2,7 @@ onmessage = function (e) {
     var xhr = new XMLHttpRequest();
     xhr.onload = function () {
         if (xhr.status === 200) {
-            postMessage(JSON.parse(xhr.responseText).content);
+            postMessage(JSON.parse(xhr.responseText));
         }
         else {
             console.log('Request failed. Returned status of ' + xhr.status);
