49921d361b988602681a13faf3f665dc6dfce95c
mocenas <33481556+mocenas@users.noreply.github.com>
2018-07-31 13:28:41 +0200

    Updated Integration test to work with up to date version of booster

--- tests/src/test/java/io/openshift/booster/OpenShiftIT.java
+++ tests/src/test/java/io/openshift/booster/OpenShiftIT.java
@@ -1,113 +1,137 @@
 package io.openshift.booster;
 
-import java.net.URL;
-import java.util.concurrent.TimeUnit;
-
-import javax.json.Json;
-
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
-import org.arquillian.cube.openshift.impl.enricher.AwaitRoute;
+import org.arquillian.cube.istio.api.IstioResource;
+import org.arquillian.cube.istio.impl.IstioAssistant;
 import org.arquillian.cube.openshift.impl.enricher.RouteURL;
 import org.jboss.arquillian.junit.Arquillian;
-import org.junit.Before;
+import org.jboss.arquillian.test.api.ArquillianResource;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.awaitility.Awaitility.await;
-import static org.hamcrest.Matchers.containsString;
-import static org.hamcrest.core.IsEqual.equalTo;
 
 /**
- * @author Radek Koubsky
- * @author Ales Justin
+ * @author Martin Ocenas
  */
 @RunWith(Arquillian.class)
+@IstioResource("classpath:gateway.yml")
 public class OpenShiftIT {
-    private static final String OK = "ok";
-    private static final String FAIL = "fail";
-    private static final String CLOSED = "closed";
-    private static final String OPEN = "open";
-    private static final String HELLO_OK = "Hello, World!";
-    private static final String HELLO_FALLBACK = "Hello, Fallback!";
+    private static final String ISTIO_NAMESPACE = "istio-system";
+    private static final String ISTIO_INGRESS_GATEWAY_NAME = "istio-ingressgateway";
 
-    // See also circuitBreaker.sleepWindowInMilliseconds
-    private static final long SLEEP_WINDOW = 5000l;
-    // See also circuitBreaker.requestVolumeThreshold
-    private static final long REQUEST_THRESHOLD = 3;
+    private static final int QUERY_ASKERS_CNT = 20;
+    private static final int QUERY_ASKERS_REQUEST_CNT = 50;
+    private static final int FALLBACK_RESPONSE_RATIO = 4; // how many passed responses should at most be passed to one fallback response
 
-    @AwaitRoute(path = "/health")
-    @RouteURL("spring-boot-istio-circuit-breaker-name")
-    private URL nameBaseUri;
+    @RouteURL(value = ISTIO_INGRESS_GATEWAY_NAME, namespace = ISTIO_NAMESPACE)
+    private URL ingressGatewayURL;
 
-    @AwaitRoute(path = "/health")
-    @RouteURL("spring-boot-istio-circuit-breaker-greeting")
-    private URL greetingBaseUri;
+    @ArquillianResource
+    private IstioAssistant istioAssistant;
 
-    @Before
-    public void setup() {
-        await().pollInterval(1, TimeUnit.SECONDS).atMost(5, TimeUnit.MINUTES).until(() -> {
-            try {
-                Response gr = RestAssured.get(greetingBaseUri + "api/ping");
-                Response nr = RestAssured.get(nameBaseUri + "api/ping");
-                return gr.getStatusCode() == 200 && nr.getStatusCode() == 200;
-            } catch (Exception ignored) {
-                return false;
-            }
-        });
+    @Test
+    public void testBasicGreeting() {
+        waitUntilApplicationIsReady();
+
+        String caller = "test";
+
+        Response response = greetingResponse(caller);
+        assertThat(response.statusCode()).isEqualTo(200);
+
+        String reponseText = response.jsonPath().get("content");
+        assertThat(reponseText).isEqualTo("Hello, World from " + caller + "!");
     }
 
     @Test
-    public void testCircuitBreaker() throws InterruptedException {
-        assertCircuitBreaker(CLOSED);
-        assertGreeting(HELLO_OK);
-        changeNameServiceState(FAIL);
-        for (int i = 0; i < REQUEST_THRESHOLD; i++) {
-            assertGreeting(HELLO_FALLBACK);
+    public void testCircuitBreakerOpen() throws IOException, InterruptedException {
+        waitUntilApplicationIsReady();
+        List <me.snowdrop.istio.api.model.IstioResource> resource = deployIstioResource("restrictive_destination_rule.yml");
+
+        Thread.sleep(TimeUnit.SECONDS.toMillis(10)); // wait for rule to take effect
+
+        ResponsesCount responsesCount = measureResponses(0);
+
+        istioAssistant.undeployIstioResources(resource);
+        // Assert that there are enough fallback responses in the responses
+        assertThat(responsesCount.getPassedResponses())
+                .isLessThan(FALLBACK_RESPONSE_RATIO * responsesCount.getFallbackResponses());
+    }
+
+    @Test
+    public void testSimulatedLoad() throws IOException, InterruptedException {
+        waitUntilApplicationIsReady();
+        List <me.snowdrop.istio.api.model.IstioResource> resource = deployIstioResource("restrictive_destination_rule.yml");
+
+        Thread.sleep(TimeUnit.SECONDS.toMillis(10)); // wait for rule to take effect
+
+        ResponsesCount responsesCount = measureResponses(150);
+
+        istioAssistant.undeployIstioResources(resource);
+        // Assert that there are enough fallback responses in the responses
+        assertThat(FALLBACK_RESPONSE_RATIO * responsesCount.getPassedResponses())
+                .isLessThanOrEqualTo(responsesCount.getFallbackResponses());
+    }
+
+    /**
+     * Create multiple threads and measure responses from the greeting service
+     * @param delay Number of milliseconds of artificial delay of one response
+     * @return Number of passed and fallback responses
+     */
+    private ResponsesCount measureResponses(int delay) throws InterruptedException {
+        // create threads that will make the calls in parallel
+        List<GreetingAsker> askerArray = new ArrayList<>();
+        for (int i=0 ; i < QUERY_ASKERS_CNT ; i++){
+            GreetingAsker asker = new GreetingAsker(Integer.toString(i),ingressGatewayURL,QUERY_ASKERS_REQUEST_CNT);
+            asker.setRequestDelay(delay);
+            asker.start();
+            askerArray.add(asker);
         }
-        // Circuit breaker should be open now
-        // Wait a little to get the current health counts - see also metrics.healthSnapshot.intervalInMilliseconds
-        await().atMost(5, TimeUnit.SECONDS).until(() -> testCircuitBreakerState(OPEN));
-        changeNameServiceState(OK);
-        // See also circuitBreaker.sleepWindowInMilliseconds
-        await().atMost(7, TimeUnit.SECONDS).pollDelay(SLEEP_WINDOW, TimeUnit.MILLISECONDS).until(() -> testGreeting(HELLO_OK));
-        // The health counts should be reset
-        assertCircuitBreaker(CLOSED);
+
+        ResponsesCount totalResponses = new ResponsesCount();
+        // wait for threats to end and take their results
+        for (GreetingAsker greetingAsker : askerArray){
+            greetingAsker.join();
+            totalResponses.addResponses(greetingAsker.getResponsesCount());
+        }
+        return totalResponses;
     }
 
-    private Response greetingResponse() {
-        return RestAssured.when().get(greetingBaseUri + "api/greeting");
+    private Response greetingResponse(String caller) {
+        return RestAssured
+                .given()
+                .baseUri(ingressGatewayURL + "breaker/greeting")
+                .param("from",caller)
+                .get("/api/greeting");
     }
 
-    private void assertGreeting(String expected) {
-        Response response = greetingResponse();
-        response.then().statusCode(200).body(containsString(expected));
+    private void waitUntilApplicationIsReady() {
+        await()
+                .pollInterval(1, TimeUnit.SECONDS)
+                .atMost(1, TimeUnit.MINUTES)
+                .untilAsserted(() ->
+                        RestAssured
+                                .given()
+                                .baseUri(ingressGatewayURL.toString())
+                                .when()
+                                .get("/breaker/greeting")
+                                .then()
+                                .statusCode(200)
+                );
     }
 
-    private boolean testGreeting(String expected) {
-        Response response = greetingResponse();
-        response.then().statusCode(200);
-        return response.getBody().asString().contains(expected);
-    }
-
-    private Response circuitBreakerResponse() {
-        return RestAssured.when().get(greetingBaseUri + "api/cb-state");
-    }
-
-    private void assertCircuitBreaker(String expectedState) {
-        Response response = circuitBreakerResponse();
-        response.then().statusCode(200).body("state", equalTo(expectedState));
-    }
-
-    private boolean testCircuitBreakerState(String expectedState) {
-        Response response = circuitBreakerResponse();
-        response.then().statusCode(200);
-        return response.getBody().asString().contains(expectedState);
-    }
-
-    private void changeNameServiceState(String state) {
-        Response response = RestAssured.given().header("Content-type", "application/json")
-                .body(Json.createObjectBuilder().add("state", state).build().toString()).put(nameBaseUri + "api/state");
-        response.then().assertThat().statusCode(200).body("state", equalTo(state));
+    private List<me.snowdrop.istio.api.model.IstioResource> deployIstioResource(String istioResource) throws IOException {
+        return istioAssistant.deployIstioResources(
+                Files.newInputStream(Paths.get("../istio/" + istioResource)));
     }
 }
