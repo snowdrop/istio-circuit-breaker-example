4f55742a836a88d93f998246f41d25ae47340bc0
Georgios Andrianakis <geoand@gmail.com>
2018-09-05 20:33:37 +0300

    Test splitting Maven commands

--- .circleci/run_tests_with_fmp.sh
+++ .circleci/run_tests_with_fmp.sh
@@ -7,6 +7,12 @@ PROJECT_ABSOLUTE_DIR=$(dirname ${SCRIPT_ABSOLUTE_DIR})
 
 pushd ${PROJECT_ABSOLUTE_DIR} > /dev/null
 
-MAVEN_OPTS="-Xmx128m"  ./mvnw clean verify -Popenshift,openshift-it "$@"
+MAVEN_OPTS="-Xmx128m" ./mvnw compile fabric8:deploy -Popenshift,openshift-it -DskipTests --projects '!tests' "$@"
+
+echo "Apps Deployed"
+echo "--------------"
+oc get pods
+
+MAVEN_OPTS="-Xmx128m" ./mvnw verify -Popenshift,openshift-it --projects 'tests' -Denv.init.enabled=false "$@"
 
 popd > /dev/null
