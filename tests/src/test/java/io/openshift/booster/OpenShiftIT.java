5fd31f18bb9b1115ce64e82f55642afdd983c0d8
mocenas <mocenas@redhat.com>
2019-04-02 13:32:55 +0200

    Refactor and expands openshift tests (#27)
    test refactor and expansion

--- tests/src/test/java/io/openshift/booster/OpenShiftIT.java
+++ tests/src/test/java/io/openshift/booster/OpenShiftIT.java
@@ -16,6 +16,7 @@ import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 import org.junit.runners.MethodSorters;
@@ -49,31 +50,41 @@ public class OpenShiftIT {
     public void testBasicGreeting() {
         waitUntilApplicationIsReady();
 
-        String caller = "test";
+        String caller = "Dwarf miner"; // use some non-common word for testing
 
         Response response = greetingResponse(caller);
         assertThat(response.statusCode()).isEqualTo(200);
 
-        String reponseText = response.jsonPath().get("content");
-        assertThat(reponseText).isEqualTo("Hello, World from " + caller + "!");
+        String responseText = response.jsonPath().get("content");
+        assertThat(responseText).isEqualTo("Hello, World from " + caller + "!");
+    }
+
+    @Test
+    public void testOneWorker() throws IOException, InterruptedException {
+        waitUntilApplicationIsReady();
+
+        ResponsesCount responsesCount = deployAndMeasure(
+                    Collections.singletonList("restrictive_destination_rule.yml"),
+                0, 1);
+        /*
+         * With one worker, there should be not fallback responses, but it is possible for some to occur.
+         */
+        assertThat(responsesCount.getFallbackResponses())
+                .isLessThan(responsesCount.getTotalResponses() / 20);
     }
 
     @Test
     public void testCircuitBreakerOpen() throws IOException, InterruptedException {
         waitUntilApplicationIsReady();
-        List <me.snowdrop.istio.api.IstioResource> resource = deployIstioResource("restrictive_destination_rule.yml");
 
-        Thread.sleep(TimeUnit.SECONDS.toMillis(10)); // wait for rule to take effect
-
-        ResponsesCount responsesCount = measureResponses(0);
-
-        istioAssistant.undeployIstioResources(resource);
-
-        // Assert that there are fallback responses
+        ResponsesCount responsesCount = deployAndMeasure(
+                Collections.singletonList("restrictive_destination_rule.yml"),
+                0, QUERY_ASKERS_CNT);
         /*
          * We cannot presume that there will be any specific number of fallback responses.
          * On high performance clusters the circuit breaker may not trip often and it could cause test to fail
          *      even if there is no real failure.
+         * But there should be at least some fallback responses, also there should be a lot of passed responses.
          */
 
         // at least 10% of responses are fallbacks
@@ -85,12 +96,9 @@ public class OpenShiftIT {
     public void testSimulatedLoad() throws IOException, InterruptedException {
         waitUntilApplicationIsReady();
 
-        List <me.snowdrop.istio.api.IstioResource> resource = deployIstioResource("restrictive_destination_rule.yml");
-        Thread.sleep(TimeUnit.SECONDS.toMillis(10)); // wait for rule to take effect
-
-        ResponsesCount responsesCount = measureResponses(150);
-
-        istioAssistant.undeployIstioResources(resource);
+        ResponsesCount responsesCount = deployAndMeasure(
+                Collections.singletonList("restrictive_destination_rule.yml"),
+                10, QUERY_ASKERS_CNT);
 
         // Assert that there are enough fallback responses in the responses
         assertThat(responsesCount.getPassedResponses())
@@ -98,14 +106,42 @@ public class OpenShiftIT {
     }
 
     /**
+     * Wait for application to become ready, deploy istio rules and measure it's effect on the application
+     *
+     * @param istioResources List of desired istio resources to deploy
+     * @param responseDelay artificiant delay inserted to each response
+     * @param workersCount number of workers which will query the application
+     * @return count of passed and failed responses
+     */
+    private ResponsesCount deployAndMeasure(List<String> istioResources,
+                                            int responseDelay,
+                                            int workersCount) throws InterruptedException, IOException {
+        waitUntilApplicationIsReady();
+
+        // deploy desired istio rules
+        List <me.snowdrop.istio.api.IstioResource> resource = new ArrayList<>();
+        for (String istioResource: istioResources){
+            resource.addAll(deployIstioResource(istioResource));
+        }
+        Thread.sleep(TimeUnit.SECONDS.toMillis(10)); // wait for rules to take effect
+
+        // measure effect on application
+        ResponsesCount responsesCount = measureResponses(responseDelay, workersCount);
+
+        istioAssistant.undeployIstioResources(resource);
+        return responsesCount;
+    }
+
+    /**
      * Create multiple threads and measure responses from the greeting service
      * @param delay Number of milliseconds of artificial delay of one response
+     * @param workersCount Number of workers to be created
      * @return Number of passed and fallback responses
      */
-    private ResponsesCount measureResponses(int delay) throws InterruptedException {
+    private ResponsesCount measureResponses(int delay, int workersCount) throws InterruptedException {
         // create threads that will make the calls in parallel
         List<GreetingWorker> workerArray = new ArrayList<>();
-        for (int i=0 ; i < QUERY_ASKERS_CNT ; i++){
+        for (int i=0 ; i < workersCount ; i++){
             GreetingWorker worker = new GreetingWorker(Integer.toString(i),ingressGatewayURL,QUERY_ASKERS_REQUEST_CNT);
             worker.setRequestDelay(delay);
             worker.start();
